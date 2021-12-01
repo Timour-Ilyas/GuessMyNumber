17d58567a6f74f8d704248c1df526aa784fb90fd
XiroD <xirodan87@gmail.com>
2021-12-01 11:58:19 +0100

    Creazione menù dell'applicazione

---
+++ app/src/main/java/com/example/guessmynumber/ActivityRecord.java
@@ -0,0 +1,14 @@
+package com.example.guessmynumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
